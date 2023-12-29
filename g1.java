055b975b0029ca190847865101ecc81639dfb72c
Pendela.Deekshita.90001338 <pendela.deekshita@microfocus.com>
2023-12-29 21:48:41 +0530

    pusging

---
+++ g1.java
@@ -0,0 +1,21 @@
+package Graphs;
+import java.util.*;
+public class g1 {
+    static class Edge{
+        int src;
+        int dest;
+        public Edge(int s,int d){
+            src=s;
+            d=dest;
+        }
+        public static void createGraph(ArrayList<Edge> graph){
+            
+
+        }
+    }
+    public static void main(String[] args) {
+         int v=4;
+         ArrayList<Edge> graph[]=new ArrayList[v];
+    }
+
+}
